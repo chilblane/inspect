2de04fcff099262e3ddf8104002ef53a59beb2cb
Daniel <daniel@chilblane.com>
2017-03-13 15:53:31 -0700

    replace armory with blizzard.js

--- app/helpers/battlenet.js
+++ app/helpers/battlenet.js
@@ -1,19 +1,18 @@
-var armory = require("armory");
-
 var _apikey = "xpbmb2crkyz7t6njsxfpvzw7v7nyssyh";
+var blizzard = require('blizzard.js').initialize({ apikey: _apikey });
 
 //TODO: locale support
 function pullCharacter(realm, name) {
-  return armory.character({
-    locale: "en_US",
-    realm: realm,
-    name: name,
-    apiKey: _apikey
-  // }, function (charData) {
-  //     console.log(charData)
-  //     // return charData.data
+  return blizzard.wow.character(
+    ['profile'],
+    {
+      origin: 'us',
+      realm: realm,
+      name: name
     }
-  )
+  ).then(function(charData) {
+    return charData.data
+  });
 }
 
 module.exports = {
