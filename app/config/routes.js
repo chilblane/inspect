ef8cb25577c42f1bcd3da37deaf968e6a81e03bb
Daniel <daniel@chilblane.com>
2017-03-13 15:56:11 -0700

    switch to browser history

--- app/config/routes.js
+++ app/config/routes.js
@@ -2,7 +2,7 @@ var React = require("react");
 var ReactRouter = require("react-router");
 var Router = ReactRouter.Router;
 var Route = ReactRouter.Route;
-var hashHistory = ReactRouter.hashHistory;
+var browserHistory = ReactRouter.browserHistory;
 var IndexRoute = ReactRouter.IndexRoute;
 
 var Main = require("../containers/Main");
@@ -10,7 +10,7 @@ var HomeContainer = require("../containers/HomeContainer");
 var CharacterPageContainer = require("../containers/CharacterPageContainer");
 
 var routes = (
-    <Router history={hashHistory}>
+    <Router history={browserHistory}>
         <Route path="/" component={Main}>
             <IndexRoute component={HomeContainer} />
             <Route path="character/:realm/:name" component={CharacterPageContainer} />
