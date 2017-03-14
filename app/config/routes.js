150fd079c3b882c0c0088bd523d3bb26d2d02e6c
Daniel <daniel@chilblane.com>
2017-03-13 21:48:44 -0700

    remove basename....

--- app/config/routes.js
+++ app/config/routes.js
@@ -6,19 +6,14 @@ var Router = ReactRouter.Router;
 var Route = ReactRouter.Route;
 var browserHistory = ReactRouter.browserHistory;
 var IndexRoute = ReactRouter.IndexRoute;
-var useRouterHistory = ReactRouter.useRouterHistory;
-var createHistory = history.createHistory;
-
-var appHistory = useRouterHistory(createHistory)({
-  basename: '/inspect'
-});
+var browserHistory = history.createBrowserHistory;
 
 var Main = require("../containers/Main");
 var HomeContainer = require("../containers/HomeContainer");
 var CharacterPageContainer = require("../containers/CharacterPageContainer");
 
 var routes = (
-    <Router history={appHistory}>
+    <Router history={browserHistory}>
         <Route path="/" component={Main}>
             <IndexRoute component={HomeContainer} />
             <Route path="character/:realm/:name" component={CharacterPageContainer} />
